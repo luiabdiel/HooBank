f3ecf8b6d89bcdab4056e2a4b50c480cbcaf6fa5
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2022-08-23 14:32:47 -0300

    feat: footer :construction:

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         {navLinks.map((nav, index) => (
           <li 
           key={nav.id} 
-          className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white`}
+          className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white hover:text-secondary`}
           >
             <a href={`#${nav.id}`}>
               {nav.title}
